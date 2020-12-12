c1db5aba96dcbbd7ce8a8be5f27805f3de2b92da
Christopher Chamberlain <chambca@gmail.com>
2020-12-12 16:13:14 -0600

    File upload test
    This is a brief file reading test. When you select an image using the <input> element, an alert tells you whether the selected file is a PNG image or not.

--- packer.js
+++ packer.js
@@ -1,3 +1,23 @@
+var pngReader = new FileReader();
+pngReader.onload = updateImageDetails;
+
+const PNG_HEADER = [137, 80, 78, 71, 13, 10, 26, 10];
+
 function test() {
-    alert('Tested!')
+    alert('Tested!');
+}
+
+function acceptImage() {
+    let img = document.getElementById('fileupload').files[0];
+    pngReader.readAsArrayBuffer(img);
+}
+
+function updateImageDetails() {
+    let buffer = new Uint8Array(pngReader.result);
+    // Quick test. Determine whether the uploaded image is a PNG or not.
+    let correctHeader = true;
+    for (let i=0; i<PNG_HEADER.length; i++) {
+        if (PNG_HEADER[i] != buffer[i]) correctHeader = false;
+    }
+    alert("This is " + (correctHeader ? "" : "not ") + "a PNG image.");
 }
